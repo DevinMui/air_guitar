9dee57e65837de3fdc3ee65fc467ad46be8dccd0
Jesse Liang <jesseliang@ymail.com>
2015-10-24 09:45:59 -0700

    thigns that are cool and stuff

---
+++ leapmotion/leapmotion/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leapmotion
+//
+//  Created by Jesse Liang on 10/24/15.
+//  Copyright © 2015 Jesse Liang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
