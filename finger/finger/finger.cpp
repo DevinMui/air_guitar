124606bda0358b14087cbdca96fa0cf4807500ae
Devin Mui <devinmui@yahoo.com>
2015-10-24 08:30:02 -0700

    got linker files working in xcode

---
+++ finger/finger/finger.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  finger
+//
+//  Created by Devin Mui on 10/24/15.
+//  Copyright © 2015 Devin Mui. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
