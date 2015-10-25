f88bbd4b887c78eb35afe4240d2b7f42c5a405fc
Devin Mui <devinmui@yahoo.com>
2015-10-24 21:13:37 -0700

    ALGORITHM FUCKING WORKS LIKE THE PIECE OF SHIT IT SHOULD HAVE

--- finger/finger/finger.cpp
+++ finger/finger/finger.cpp
@@ -162,11 +162,116 @@ void SampleListener::onExit(const Controller& controller) {
 }
 
 void SampleListener::onFrame(const Controller& controller) {
+    const Frame frame = controller.frame();
+    HandList hands = frame.hands();
     try {
-        // First, we create a Hub with our application identifier. Be sure not to use the com.example namespace when
-        // publishing your application. The Hub provides access to one or more Myos.
         myo::Hub hub("io.github.devinmui.finger");
+        std::cout << "Attempting to find a Myo..." << std::endl;
+            
+        myo::Myo* myo = hub.waitForMyo(10000);
+            
+        if (!myo) {
+            throw std::runtime_error("Unable to find a Myo!");
+        }
+            
+        std::cout << "Connected to a Myo armband!" << std::endl << std::endl;
+    
         
+        DataCollector collector;
+            
+        hub.addListener(&collector);
+            
+        float pitch = collector.pitch_w;
+        float yaw = collector.yaw_w;
+                
+        for (HandList::const_iterator hl = hands.begin(); hl != hands.end(); hl++) {
+            const Hand hand = *hl;
+            hub.run(1000/20);
+            double foo = hand.palmPosition()[2];
+            std::cout << std::to_string(foo) << std::endl;
+            std::string pose = collector.currentPose.toString();
+            float init_pitch = collector.pitch_w;
+            float init_yaw = collector.yaw_w;
+            float move_pitch = 0;
+            float move_yaw = 0;
+            if(init_pitch - pitch < 0) {
+                move_pitch = (init_pitch - pitch) * -1;
+            } else {
+                move_pitch = init_pitch - pitch; // calculate the movement of the pitch
+            }
+            if(init_yaw - yaw < 0) {
+                move_yaw = (init_yaw - yaw) * -1; // calculate the movement of the yaw
+            } else {
+                move_yaw = init_yaw - yaw;
+            }
+        
+            // after calculations
+            if(move_pitch >= 1) {
+                std::cout << "YES" << std::endl;
+            } else {
+                std::cout << "NO" << std::endl;
+            }
+                    
+            //std::cout << "pitch: " << init_pitch << ", yaw: "<< init_yaw << std::endl;
+            pitch = init_pitch; // pitch should be around ~ 5+ difference
+            yaw = init_yaw; // yaw should be 1 - 2 difference
+            // whatever might not need yaw or roll just do pitch
+                    
+            if(pose == "fist"){
+                std::cout << "FISTBUMP!" << std::endl;
+                // play music based on calculations from leap motion
+            } else {
+                std::cout << "No fistbump :(" << std::endl;
+            }
+        }
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        std::cerr << "Press enter to continue.";
+        std::cin.ignore()
+        ;
+        //return -1;
+    }
+}
+
+    
+
+
+
+
+void SampleListener::onFocusGained(const Controller& controller) {
+    std::cout << "Focus Gained" << std::endl;
+}
+
+void SampleListener::onFocusLost(const Controller& controller) {
+    std::cout << "Focus Lost" << std::endl;
+}
+
+void SampleListener::onDeviceChange(const Controller& controller) {
+    std::cout << "Device Changed" << std::endl;
+    const DeviceList devices = controller.devices();
+    
+    for (int i = 0; i < devices.count(); ++i) {
+        std::cout << "id: " << devices[i].toString() << std::endl;
+        std::cout << "  isStreaming: " << (devices[i].isStreaming() ? "true" : "false") << std::endl;
+    }
+}
+
+void SampleListener::onServiceConnect(const Controller& controller) {
+    std::cout << "Service Connected" << std::endl;
+}
+
+void SampleListener::onServiceDisconnect(const Controller& controller) {
+    std::cout << "Service Disconnected" << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+    SampleListener listener;
+    Controller controller;
+    
+    
+    try {
+        myo::Hub hub("io.github.devinmui.finger");
         std::cout << "Attempting to find a Myo..." << std::endl;
         
         myo::Myo* myo = hub.waitForMyo(10000);
@@ -177,16 +282,21 @@ void SampleListener::onFrame(const Controller& controller) {
         
         std::cout << "Connected to a Myo armband!" << std::endl << std::endl;
         
+        
         DataCollector collector;
         
         hub.addListener(&collector);
         
         float pitch = collector.pitch_w;
         float yaw = collector.yaw_w;
-        const Frame frame = controller.frame();
-        
-        HandList hands = frame.hands();
-        while(1) {
+        while(1){
+            const Frame frame = controller.frame();
+            HandList hands = frame.hands();
+            for (HandList::const_iterator hl = hands.begin(); hl != hands.end(); hl++) {
+                const Hand hand = *hl;
+                double foo = hand.palmPosition()[2];
+                std::cout << std::to_string(foo) << std::endl;
+            }
             hub.run(1000/20);
             std::string pose = collector.currentPose.toString();
             float init_pitch = collector.pitch_w;
@@ -222,73 +332,15 @@ void SampleListener::onFrame(const Controller& controller) {
             } else {
                 std::cout << "No fistbump :(" << std::endl;
             }
-
-            // this doesnt execute...
-            for (HandList::const_iterator hl = hands.begin(); hl != hands.end(); ++hl) {
-                
-                const Hand hand = *hl;
-            
-                std::cout << hand.palmPosition()[2] << std::endl;
-            }
-        }
         
-        // If a standard exception occurred, we print out its message and exit.
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        std::cerr << "Press enter to continue.";
-        std::cin.ignore();
-        //return -1;
-    }
+        }
+        } catch (const std::exception& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+            std::cerr << "Press enter to continue.";
+            std::cin.ignore()
+            ;
+            return -1;
+        }
     
-}
-
-
-
-void SampleListener::onFocusGained(const Controller& controller) {
-    std::cout << "Focus Gained" << std::endl;
-}
-
-void SampleListener::onFocusLost(const Controller& controller) {
-    std::cout << "Focus Lost" << std::endl;
-}
-
-void SampleListener::onDeviceChange(const Controller& controller) {
-    std::cout << "Device Changed" << std::endl;
-    const DeviceList devices = controller.devices();
-    
-    for (int i = 0; i < devices.count(); ++i) {
-        std::cout << "id: " << devices[i].toString() << std::endl;
-        std::cout << "  isStreaming: " << (devices[i].isStreaming() ? "true" : "false") << std::endl;
-    }
-}
-
-void SampleListener::onServiceConnect(const Controller& controller) {
-    std::cout << "Service Connected" << std::endl;
-}
-
-void SampleListener::onServiceDisconnect(const Controller& controller) {
-    std::cout << "Service Disconnected" << std::endl;
-}
-
-int main(int argc, char** argv)
-{
-    SampleListener listener;
-    Controller controller;
-    
-    // Have the sample listener receive events from the controller
-    controller.addListener(listener);
-    
-    if (argc > 1 && strcmp(argv[1], "--bg") == 0)
-        controller.setPolicy(Leap::Controller::POLICY_BACKGROUND_FRAMES);
-    
-    // Keep this process running until Enter is pressed
-    std::cout << "Press Enter to quit..." << std::endl;
-    std::cin.get();
-    
-    // Remove the sample listener when done
-    controller.removeListener(listener);
-    
-    return 0;
-
 }
 
